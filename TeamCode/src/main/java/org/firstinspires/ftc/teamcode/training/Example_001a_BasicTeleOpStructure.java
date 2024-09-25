ef3d50c8a9400564b75698b4168d64796ac5542f
vector5233- Mr. F <vector5233@gmail.com>
2024-09-25 15:57:48 -0400

    Training 101

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/Example_001a_BasicTeleOpStructure.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.training;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**Configuration file
+ *
+ */
+
+@Disabled
+@TeleOp(group = "Primary", name = "ShorName")
+public class Example_001a_BasicTeleOpStructure extends LinearOpMode {
+    @Override
+    public void  runOpMode() throws InterruptedException{
+        initHardware();
+        while(!isStarted()){}
+        waitForStart();
+        while(opModeIsActive()){}
+    }
+    public void initHardware(){}
+}
