4e73140f95f805494b61ca55ba7a41864bdad066
Kirk Frankfurth <kfrankfurth@chapelgateacademy.org>
2024-09-26 20:50:27 -0400

    Third File Im Uploading for training

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/Example_001c_ServoOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/Example_001c_ServoOne.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.training;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -9,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Servo Port 00: servoOne // must match variable name in CH and in Java
  */
 
-//@Disabled
+@Disabled
 @TeleOp(group = "Primary", name = "Servo with buttons")
 public class Example_001c_ServoOne extends LinearOpMode {
 
@@ -37,7 +38,9 @@ public class Example_001c_ServoOne extends LinearOpMode {
 
 
         }
+
     }
+
     public void initHardware(){
         initServoOne(); // call to the method other methods/ hardware pieces - method stacking
 
