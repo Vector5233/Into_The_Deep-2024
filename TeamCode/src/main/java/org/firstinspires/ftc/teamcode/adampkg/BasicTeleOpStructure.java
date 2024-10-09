57c2a21ffea84917b8db2128d61a77291376d838
Vector C 5233 <vector5233@gmail.com>
2024-10-09 08:43:14 -0400

    THis is the basic teleop structure.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adampkg/BasicTeleOpStructure.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.adampkg;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**Config File
+ * Port 00: motorOne
+ * Port 01: motorTwo
+ * Port 02: motorThree
+ * Port 00: Servo servoOne
+ * Port 01: Servo servoTwo
+ * Port 02: CRServo servoThree
+ */
+//@Disabled
+@TeleOp(group = "Qureshi", name = "BasicTeleop")
+public class BasicTeleOpStructure extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+    initHardware();
+    while(!isStarted())
+    {
+        //camera methods that
+    }
+    waitForStart();
+    while(opModeIsActive())
+    {
+        //methods
+    }
+
+    }
+    public void initHardware()
+    {
+
+    }
+
+}
