1f8068c396cb64f5846110b50aa6471975f06438
vector5233- Mr. F <vector5233@gmail.com>
2024-10-10 16:38:14 -0400

    Field Centric Drive teleop added in imu and a toggle for yaw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/FieldCentricDrive.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode.training;
 
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+import java.util.concurrent.TimeUnit;
 
-/**Configuration file
+/**
+ * Configuration file
  * Motor port 1 : leftFM
  * Motor port 2 : rightFM
  * Motor port 3 : leftBM
@@ -19,43 +26,75 @@ public class FieldCentricDrive extends LinearOpMode {
     private DcMotor rightFM;
     private DcMotor leftBM;
     private DcMotor rightBM;
-
-    double lx =gamepad1.left_stick_x;
+    private IMU imu;
+    double lx = gamepad1.left_stick_x;
     double ly = gamepad1.left_stick_y;
     double rx = gamepad1.right_stick_x;
+
+    Deadline gamepadRateLimit = new Deadline(500, TimeUnit.MILLISECONDS);
+
     @Override
-    public void  runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
         initHardware();
-        while(!isStarted()){}
+        while (!isStarted()) {
+        }
+
+        initIMU();
+
         waitForStart();
-        while(opModeIsActive()){
 
-            double max = Math.max(Math.abs(lx)+ Math.abs(ly) + Math.abs(rx), 1);
-            double power = 0.2 + (0.6 * gamepad1.right_trigger);
 
-        leftFM.setPower(((ly+lx+rx)/max)* power);
-        leftBM.setPower(((ly-lx+rx)/max)* power);
-        rightFM.setPower(((ly-lx-rx)/max)* power);
-        rightBM.setPower(((ly+lx-rx)/max)* power);
+        while (opModeIsActive()) {
 
+            teleOpControls();
 
 
         }
     }
-    public void initHardware(){
+
+    public void initHardware() {
+        initIMU();
         initDriveMotors();
 
     }
-    public void initDriveMotors(){
-        leftFM = hardwareMap.get(DcMotor.class,"leftFM");
-        rightFM = hardwareMap.get(DcMotor.class,"rightFM");
-        leftBM = hardwareMap.get(DcMotor.class,"leftBM");
-        rightBM = hardwareMap.get(DcMotor.class,"rightBM");
+
+    public void initDriveMotors() {
+        leftFM = hardwareMap.get(DcMotor.class, "leftFM");
+        rightFM = hardwareMap.get(DcMotor.class, "rightFM");
+        leftBM = hardwareMap.get(DcMotor.class, "leftBM");
+        rightBM = hardwareMap.get(DcMotor.class, "rightBM");
 
         // set oneside to reverse
         leftFM.setDirection(DcMotor.Direction.REVERSE);
         leftBM.setDirection(DcMotor.Direction.REVERSE);
 
 
+    }
+
+    public void initIMU() {
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
+
+
+    }
+
+    public void teleOpControls() {
+        double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+        double power = 0.8 + (0.6 * gamepad1.right_trigger);
+
+        if (gamepadRateLimit.hasExpired() && gamepad1.a) {
+            imu.resetYaw();
+            gamepadRateLimit.reset();
+        }
+
+        double heading = -imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double adjustedLx = -ly * Math.sin(heading) + lx * Math.cos(heading);
+        double adjustedLy = ly * Math.cos(heading) + lx * Math.sin(heading);
+
+        leftFM.setPower(((adjustedLy + adjustedLx + rx) / max) * power);
+        leftBM.setPower(((adjustedLy - adjustedLx + rx) / max) * power);
+        rightFM.setPower(((adjustedLy - adjustedLx - rx) / max) * power);
+        rightBM.setPower(((adjustedLy + adjustedLx - rx) / max) * power);
     }
 }
