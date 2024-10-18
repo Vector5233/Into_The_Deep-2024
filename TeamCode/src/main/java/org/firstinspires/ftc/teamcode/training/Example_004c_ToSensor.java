cabde809d3ccc0a0db6368c0b1c85019e1916e2a
kfrankfurth <vector5233@gmail.com>
2024-10-18 14:47:58 -0400

    Distance Sensor Fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/Example_004c_ToSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/training/Example_004c_ToSensor.java
@@ -1,70 +1,77 @@
 package org.firstinspires.ftc.teamcode.training;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-/**Configuration file
- *
+/**
+ * Configuration file
+ * Digital port 01: touchSensor defaults to the higher number if one sensor.
  */
 
-@Disabled
+//@Disabled
 @TeleOp(group = "Primary", name = "Touch Sensor")
 public class Example_004c_ToSensor extends LinearOpMode {
     private TouchSensor touchSensor;
-    boolean touchSensorIsPressed = true;
+    boolean touchSensorIsPressed = false;
     double touchSensorValue;
 
     private Servo servoOne;
-    double servoOneInitPosition =0.5;
-    double servoOnePositionOne =0.0;
-    double servoOnePositionTwo= 1.0;
+    double servoOneInitPosition = 0.5;
+    double servoOnePositionOne = 0.0;
+    double servoOnePositionTwo = 1.0;
 
     @Override
-    public void  runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
         initHardware();
-        while(!isStarted()){
+        while (!isStarted()) {
+            getTouchSensor();
             switchTelemetry();
 
         }
         waitForStart();
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
             getTouchSensor();
             switchTelemetry();
             teleOpControls();
         }
     }
-    public void initHardware(){
-        getTouchSensor();
-        initTouchSensor();
+
+    public void initHardware() {
         initServoOne();
+        initTouchSensor();
+
     }
-    public void initServoOne(){
+
+    public void initServoOne() {
         servoOne = hardwareMap.get(Servo.class, "servoOne");
         servoOne.setDirection(Servo.Direction.FORWARD);
         servoOne.setPosition(servoOneInitPosition);
     }
 
-    public void initTouchSensor(){
-        touchSensor=hardwareMap.get(TouchSensor.class, "touchSensor");
+    public void initTouchSensor() {
+
+        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
     }
-    public void getTouchSensor(){
+
+    public void getTouchSensor() {
         touchSensorIsPressed = touchSensor.isPressed();
         touchSensorValue = touchSensor.getValue();
     }
-    public  void switchTelemetry(){
+
+    public void switchTelemetry() {
         telemetry.addData("Touch Sensor pressed", touchSensorIsPressed);
         telemetry.addData("Touch Sensor Value", "%2f", touchSensorValue);
-        telemetry.addData("servoOnePosition","%2f", servoOne.getPosition());
+        telemetry.addData("servoOnePosition", "%2f", servoOne.getPosition());
         telemetry.update();
 
     }
-    public void teleOpControls(){
-        if(touchSensorIsPressed){
+
+    public void teleOpControls() {
+        if (touchSensorIsPressed) {
             servoOne.setPosition(servoOnePositionTwo);
-        }else {
+        } else {
             servoOne.setPosition(servoOnePositionOne);
         }
 
