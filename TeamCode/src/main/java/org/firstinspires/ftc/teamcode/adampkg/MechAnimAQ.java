06bbdad2abe30a3f46c1d3c73bc4ec3fe8aa9ab0
Vector C 5233 <vector5233@gmail.com>
2024-11-13 09:12:01 -0500

    Save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adampkg/MechAnimAQ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adampkg/MechAnimAQ.java
@@ -1,3 +1,4 @@
+
 package org.firstinspires.ftc.teamcode.adampkg;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -11,24 +12,24 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  * Port 01: motorTwo MFR
  * Port 02: motorThree MBL
  * Port 03: motorFour MBR
+ *
  * Port 00: Servo servoOne
  * Port 01: Servo servoTwo
  * Port 02: CRServo servoThree
+ *
  */
 //@Disabled
 @TeleOp(group = "Qureshi", name = "BasicTeleop")
 public class MechAnimAQ extends LinearOpMode {
-
-    DcMotor dcMotorMFL;
-    DcMotor dcMotorMFR;
-    DcMotor dcMotorMBL;
-    DcMotor dcMotorMBR;
+    double liftDirection = 0;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    DcMotor liftRight;
+    DcMotor liftLeft;
 
     //a game pad = a controller
-    double lx = gamepad1.left_stick_x;
-    double ly = gamepad1.left_stick_y;
-    double rx = gamepad1.right_stick_x;
-    double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -41,21 +42,55 @@ public class MechAnimAQ extends LinearOpMode {
         }
 
     }
+    public void Lift()
+    {
+        if(gamepad1.y)
+        {
+            liftDirection = 1;
+        }
+        else if(gamepad1.a)
+        {
+            liftDirection = -1;
+        }
+        else {
+            liftDirection = 0;
+        }
+        liftRight.setPower(liftDirection);
+        liftLeft.setPower(liftDirection);
 
+        //return liftDirection;
+    }
     public void initHardware() {
-        dcMotorMFL = hardwareMap.dcMotor.get("MFL");
-        dcMotorMFR = hardwareMap.dcMotor.get("MFR");
-        dcMotorMBL = hardwareMap.dcMotor.get("MBL");
-        dcMotorMBR = hardwareMap.dcMotor.get("MBR");
+        initDrive();
+    }
 
-        dcMotorMBL.setDirection(DcMotorSimple.Direction.REVERSE);
-        dcMotorMFL.setDirection(DcMotorSimple.Direction.REVERSE);
+    private void initDrive() {
+        frontLeft = hardwareMap.get(DcMotor.class,"leftFront");
+        frontRight = hardwareMap.get(DcMotor.class,"rightFront");
+        backLeft = hardwareMap.get(DcMotor.class, "leftBack");
+        backRight = hardwareMap.get(DcMotor.class,"rightBack");
+        liftRight = hardwareMap.get(DcMotor.class, "rightLift");
+        liftLeft = hardwareMap.get(DcMotor.class, "leftLift");
+        liftLeft.setDirection(DcMotor.Direction.REVERSE);
+        // backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+    public void driveTrain()
+    {
+        double lx = gamepad1.left_stick_x;
+        double ly = -gamepad1.left_stick_y;
+        double rx = gamepad1.right_stick_x;
+        double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+
+        frontLeft.setPower((ly + lx + rx) / max);
+        frontRight.setPower((ly - lx - rx) / max);
+        backLeft.setPower((ly - lx + rx) / max);
+        backRight.setPower((ly + lx - rx) / max);
     }
 
     public void teleOpControls() {
-        dcMotorMFL.setPower((ly + lx + rx) / max);
-        dcMotorMBL.setPower((ly - lx + rx) / max);
-        dcMotorMFR.setPower((ly - lx - rx) / max);
-        dcMotorMBR.setPower((ly + lx - rx) / max);
+        driveTrain();
+        Lift();
+        liftDirection = 0;
     }
 }
