43200543dbce33694d2007a107e922bc63f532e1
Vector C 5233 <vector5233@gmail.com>
2024-11-14 15:34:20 -0500

    Servo Addition Pincher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adampkg/MechAnimAQ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adampkg/MechAnimAQ.java
@@ -4,7 +4,8 @@ package org.firstinspires.ftc.teamcode.adampkg;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
 
 /**
  * Config File
@@ -29,6 +30,13 @@ public class MechAnimAQ extends LinearOpMode {
     DcMotor liftRight;
     DcMotor liftLeft;
 
+
+    private Servo servoPincher; // servos go from 0 to 1 rotates 180 degrees
+    double servoPincherInitPosition = 0.5; // doubles store a decimal
+    double servoPincherPositionOpen = 0.0;
+    double servoPincherPositionClose = 1.0;
+    int servoOneDelay = 10;
+
     //a game pad = a controller
 
     @Override
@@ -38,6 +46,7 @@ public class MechAnimAQ extends LinearOpMode {
         }
         waitForStart();
         while (opModeIsActive()) {
+            servoTelemetry();
             teleOpControls();
         }
 
@@ -62,6 +71,7 @@ public class MechAnimAQ extends LinearOpMode {
     }
     public void initHardware() {
         initDrive();
+        initServoOne();
     }
 
     private void initDrive() {
@@ -75,6 +85,24 @@ public class MechAnimAQ extends LinearOpMode {
         // backLeft.setDirection(DcMotor.Direction.REVERSE);
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
     }
+    public void initServoOne() {
+        servoPincher = hardwareMap.get(Servo.class, "servoPincher"); // maps the servo
+        servoPincher.setDirection(Servo.Direction.FORWARD); // sets the direction of rotation - optional but good practice
+        servoPincher.setPosition(servoPincherInitPosition); // sets the initial position from the variable above.
+    }
+    public void ServoMovement()
+    {
+        if(gamepad1.right_bumper)
+        {
+            servoPincher.setPosition(servoPincherPositionOpen);
+        }
+        if(gamepad1.left_bumper)
+        {
+            servoPincher.setPosition(servoPincherPositionClose);
+        }
+
+    }
+
     public void driveTrain()
     {
         double lx = gamepad1.left_stick_x;
@@ -87,9 +115,21 @@ public class MechAnimAQ extends LinearOpMode {
         backLeft.setPower((ly - lx + rx) / max);
         backRight.setPower((ly + lx - rx) / max);
     }
-
+    public void servoTelemetry() {
+        //telemetry.log().clear();
+        telemetry.addData("Position", servoPincher.getPosition());
+        telemetry.addData("Direction", servoPincher.getDirection());
+        telemetry.addData("Controller", servoPincher.getController());
+        telemetry.addData("Port Number", servoPincher.getConnectionInfo());
+        telemetry.addData("Device Name", servoPincher.getDeviceName());
+        telemetry.addData("Manufacture", servoPincher.getManufacturer());
+        telemetry.addData("Version", servoPincher.getVersion());
+        telemetry.addData("Class", servoPincher.getClass());
+        telemetry.update();
+    }
     public void teleOpControls() {
         driveTrain();
+        ServoMovement();
         Lift();
         liftDirection = 0;
     }
