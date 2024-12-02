d44b2ff983ba657fd48b68778ad11bc1fc8715e6
kfrankfurth <vector5233@gmail.com>
2024-12-02 08:35:51 -0500

    Main Directory File

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTONOMOUS/BLUELEFT.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.AUTONOMOUS;
+
+public class BLUELEFT {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP/TELEOP_MASTER.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.TELEOP;
+
+public interface TELEOP_MASTER {
+}
