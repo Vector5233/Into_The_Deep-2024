4e729927d84a83c958f855563c32c0218ce581ae
vector5233- Mr. F <vector5233@gmail.com>
2025-04-08 14:22:09 -0400

    Fixed build gradle issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NEFARIO/SensorPinpointDriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NEFARIO/SensorPinpointDriveToPoint.java
@@ -1,15 +1,14 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.NEFARIO;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
 
 import java.util.Locale;
 
